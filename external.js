ddca90b1e30e1bf33a98bb759114509444ed2ae0
Soojin Kim <soojinkk98@gmail.com>
2022-05-31 12:59:17 -0400

    calculator will be able to support decimals

--- external.js
+++ external.js
@@ -6,13 +6,13 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-    output = num1 - num2;
+    output = Number(num1) - Number(num2);
     num1 = output;
     return output;
 }
 
 function multiply(num1, num2) {
-    output = num1 * num2;
+    output = Number(num1) * Number(num2);
     num1 = output;
     return output;
 }
@@ -21,7 +21,7 @@ function divide(num1, num2) {
     if (num2 == 0) {
         alert("CANNOT DIVIDE By ZERO");
     }
-    output = num1 / num2;
+    output = Number(num1) / Number(num2);
     num1 = output;
     return output;
 }
@@ -34,34 +34,51 @@ function operate(operator, num1, num2) {
 
 const btns = document.querySelectorAll('button');
 let output;
-let num1;
-let num2;
+let num1 = '';
+let num2 = '';
 let operator;
+let display;
+let firstnum = true;
 
 
 btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        if (!isNaN(e.target.value)) {
-            document.querySelector('#display').innerHTML = e.target.value;
-            if (!num1) {
-                num1 = e.target.value;
+        if (!isNaN(e.target.value) || e.target.value == 'dot') {
+            if (firstnum) {
+                if (e.target.value == 'dot') {
+                    num1 += '.';
+                }
+                else {
+                    num1 += e.target.value;
+                }
+                display = num1;
             }
             else {
-                num2 = e.target.value;
+                if  (e.target.value == 'dot') {
+                    num2 += '.';
+                }
+                else {
+                    num2 += e.target.value;
+                }
+                display = num2;
             }
+            document.querySelector('#display').innerHTML = display;
         }
         else if (e.target.value == 'operate') {
             num1 = window[e.target.value](operator, num1, num2);
-
+            firstnum = true;
+            num2 = '';
         }
         else if (e.target.value == 'clear') {
             document.querySelector('#display').innerHTML = "";
-            num1 = null;
-            num2 = null;
+            num1 = '';
+            num2 = '';
             operator = null;
+            firstnum = true;
         }
         else {
             operator = e.target.value;
+            firstnum = false;
         }
             
     });
